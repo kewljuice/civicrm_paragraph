a739574764705555fb3853115d89dbf9a87ff77b
wouter.h <wouter@ctrl.be>
2018-02-07 17:35:30 +0100

    refs #optin add form + submission for anonymous users.

--- civicrm_paragraph_optin/templates/civicrm_paragraph_optin_anom.tpl.php
+++ civicrm_paragraph_optin/templates/civicrm_paragraph_optin_anom.tpl.php
@@ -1,11 +1,11 @@
 <div class="optin-civicrm">
     <div class="optin-item">
         <div class="optin-item-title">
-          <?php print t('Manage your newsletter subscriptions'); ?>
+          <?php print t('Subscribe to our newsletter'); ?>
         </div>
         <div class="optin-item-content">
           <?php
-          $form = drupal_get_form('civicrm_paragraph_optin_auth_form', $object);
+          $form = drupal_get_form('civicrm_paragraph_optin_anom_form', $object);
           print drupal_render($form);
           ?>
         </div>
